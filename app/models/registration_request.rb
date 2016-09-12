c4b1cc30f59e006bc3e96b865ef3d82cbf17d0e0
jburl <jake.burlingham@gmail.com>
2016-09-11 18:14:05 -0700

    autoincrement request_uid

--- app/models/registration_request.rb
+++ app/models/registration_request.rb
@@ -1,2 +1,9 @@
 class RegistrationRequest < ActiveRecord::Base
+	before_create :set_request_uid
+	
+	private
+	def set_request_uid
+		last_val = RegistrationRequest.maximum(:request_uid)
+		self.request_uid = last_val.to_i + 1
+	end
 end
